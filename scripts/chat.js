341c6c270b0e97b2d7d233ecfcc2b6f2d2ed5225
Tony Zito <178870+tonyrobots@users.noreply.github.com>
2024-02-12 16:59:27 -0800

    To facilitate use in funnels, when you might not have tokens for each character in a scene, added support for attack rolls without a token, assuming the automate friendly fire and auto-range check options aren't selected (since those require a token.)

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,183 +1,214 @@
 /* global canvas, game, fromUuidSync, Roll, socketlib */
 
-import DCCQOL from './patch.js'
+import DCCQOL from "./patch.js";
 
-export let socketlibSocket
+export let socketlibSocket;
 
-async function applyDamageQOL (targettoken, damage) {
-  let targetActor
+async function applyDamageQOL(targettoken, damage) {
+  let targetActor;
   if (targettoken) {
-    game.user.updateTokenTargets([targettoken._id])
-    targetActor = game.canvas.tokens.get(targettoken._id).actor
+    game.user.updateTokenTargets([targettoken._id]);
+    targetActor = game.canvas.tokens.get(targettoken._id).actor;
   } else {
-    targetActor = game.user.targets.first().actor
+    targetActor = game.user.targets.first().actor;
   }
   await targetActor.update({
-    'data.attributes.hp.value': targetActor.system.attributes.hp.value - damage
-  })
+    "data.attributes.hp.value": targetActor.system.attributes.hp.value - damage,
+  });
 }
 
 export const setupSocket = () => {
-  socketlibSocket = socketlib.registerModule('dcc-qol')
-  socketlibSocket.register('applyDamageQOL', applyDamageQOL)
+  socketlibSocket = socketlib.registerModule("dcc-qol");
+  socketlibSocket.register("applyDamageQOL", applyDamageQOL);
+};
+
+export function addChatListeners(html) {
+  html.on("click", ".card-buttons button", ChatCardAction);
+  html.on("click", ".card-header", ChatCardToggleContent);
 }
 
-export function addChatListeners (html) {
-  html.on('click', '.card-buttons button', ChatCardAction)
-  html.on('click', '.card-header', ChatCardToggleContent)
+async function ChatCardToggleContent(event) {
+  event.preventDefault();
+  const header = event.currentTarget;
+  const card = header.closest(".chat-card");
+  const content = card.querySelector(".card-content");
+  content.style.display = content.style.display === "none" ? "block" : "none";
 }
 
-async function ChatCardToggleContent (event) {
-  event.preventDefault()
-  const header = event.currentTarget
-  const card = header.closest('.chat-card')
-  const content = card.querySelector('.card-content')
-  content.style.display = content.style.display === 'none' ? 'block' : 'none'
-}
+async function ChatCardAction(event) {
+  const button = event.currentTarget;
+  button.disabled = true;
+  const card = button.closest(".chat-card");
+  const messageId = card.closest(".message").dataset.messageId;
+  const message = game.messages.get(messageId);
+  const action = button.dataset.action;
+  button.disabled = false;
+  const actor = game.actors.get(card.dataset.actorId) || null;
 
-async function ChatCardAction (event) {
-  const button = event.currentTarget
-  button.disabled = true
-  const card = button.closest('.chat-card')
-  const messageId = card.closest('.message').dataset.messageId
-  const message = game.messages.get(messageId)
-  const action = button.dataset.action
-  button.disabled = false
-  // const actor = game.actors.get(card.dataset.actorId) || null
-  const actor = fromUuidSync(card.dataset.tokenId).actor || null
-  const messagecreator = message._source.user
+  const messagecreator = message._source.user;
 
-  if (game.user._id !== messagecreator && !game.user.isGM) return
+  if (game.user._id !== messagecreator && !game.user.isGM) return;
 
-  if (!actor) return
+  if (!actor) return;
 
-  const act = new game.dcc.DCCActor(actor)
-  const DCCQOLactor = new DCCQOL(actor)
+  const act = new game.dcc.DCCActor(actor);
+  const DCCQOLactor = new DCCQOL(actor);
 
-  const options = JSON.parse(card.dataset.options)
-  const weapon = actor.items.get(card.dataset.weaponId)
+  const options = JSON.parse(card.dataset.options);
+  const weapon = actor.items.get(card.dataset.weaponId);
 
-  const targettokenId = card.dataset.targettokenId
-  const targettoken = fromUuidSync(targettokenId)
+  const targettokenId = card.dataset.targettokenId;
+  const targettoken = fromUuidSync(targettokenId);
 
-  const controlledTokens = canvas.tokens.controlled
-  let cToken
+  const controlledTokens = canvas.tokens.controlled;
+  let cToken;
 
   switch (action) {
-    case 'damage':
-
-      const damageRollResult = await act.rollDamage(weapon, options)
-      let targetActor
+    case "damage":
+      const damageRollResult = await act.rollDamage(weapon, options);
+      let targetActor;
 
       if (damageRollResult.damage < 1) {
-        damageRollResult.damage = 1
-        damageRollResult.roll._total = 1
+        damageRollResult.damage = 1;
+        damageRollResult.roll._total = 1;
       }
 
-      let diceHTML = await damageRollResult.roll.render()
+      let diceHTML = await damageRollResult.roll.render();
 
-      if ((targettoken) || (Array.from(game.user.targets).length === 1)) {
+      if (targettoken || Array.from(game.user.targets).length === 1) {
         if (targettoken) {
-          game.user.updateTokenTargets([targettoken._id])
-          targetActor = targettoken.actor
+          game.user.updateTokenTargets([targettoken._id]);
+          targetActor = targettoken.actor;
         } else {
-          targetActor = game.user.targets.first().actor
+          targetActor = game.user.targets.first().actor;
         }
 
-        diceHTML = diceHTML + '<br/>' + game.i18n.format('DCC-QOL.TakeDamage', { actor: targetActor.name, damage: damageRollResult.damage })
+        // show different messages depending on automateDamageApply setting
+        if (game.settings.get("dcc-qol", "automateDamageApply")) {
+          diceHTML =
+            diceHTML +
+            "<br/>" +
+            game.i18n.format("DCC-QOL.TakeDamage", {
+              actor: targetActor.name,
+              damage: damageRollResult.damage,
+            });
+        } else {
+          diceHTML =
+            diceHTML +
+            "<br/>" +
+            game.i18n.format("DCC-QOL.TakeDamageManual", {
+              actor: targetActor.name,
+              damage: damageRollResult.damage,
+            });
+        }
 
         const msg = await damageRollResult.roll.toMessage({
           user: game.user.id,
           speaker: {
-            alias: actor.name
+            alias: actor.name,
           },
           content: diceHTML,
-          flavor: game.i18n.format('DCC-QOL.DamageRoll', {
-            weapon: weapon.name
+          flavor: game.i18n.format("DCC-QOL.DamageRoll", {
+            weapon: weapon.name,
           }),
           flags: {
-            'dcc.RollType': 'Damage',
-            'dcc.ItemId': options.weaponId
-          }
-        })
+            "dcc.RollType": "Damage",
+            "dcc.ItemId": options.weaponId,
+          },
+        });
 
-        if (game.settings.get('dcc-qol', 'automateDamageApply')) {
+        if (game.settings.get("dcc-qol", "automateDamageApply")) {
           /* Update HP only after Dice So Nice animation finished */
-          if (game.modules.get('dice-so-nice')?.active) {
-            game.dice3d.waitFor3DAnimationByMessageID(msg.id).then(() => socketlibSocket.executeAsGM('applyDamageQOL', targettoken, damageRollResult.damage))
+          if (game.modules.get("dice-so-nice")?.active) {
+            game.dice3d
+              .waitFor3DAnimationByMessageID(msg.id)
+              .then(() =>
+                socketlibSocket.executeAsGM(
+                  "applyDamageQOL",
+                  targettoken,
+                  damageRollResult.damage
+                )
+              );
           } else {
-            await socketlibSocket.executeAsGM('applyDamageQOL', targettoken, damageRollResult.damage)
+            await socketlibSocket.executeAsGM(
+              "applyDamageQOL",
+              targettoken,
+              damageRollResult.damage
+            );
           }
         }
       } else {
         await damageRollResult.roll.toMessage({
           user: game.user.id,
           speaker: {
-            alias: actor.name
+            alias: actor.name,
           },
           content: diceHTML,
-          flavor: game.i18n.format('DCC-QOL.DamageRoll', {
-            weapon: weapon.name
+          flavor: game.i18n.format("DCC-QOL.DamageRoll", {
+            weapon: weapon.name,
           }),
           flags: {
-            'dcc.RollType': 'Damage',
-            'dcc.ItemId': options.weaponId
-          }
-        })
+            "dcc.RollType": "Damage",
+            "dcc.ItemId": options.weaponId,
+          },
+        });
       }
 
-      break
-    case 'fumble':
-      cToken = canvas.tokens.get(fromUuidSync(card.dataset.tokenId)._id)
-      cToken.control({ releaseOthers: true })      
-      await act.rollFumble(options)
-      canvas.tokens.selectObjects()      
+      break;
+    case "fumble":
+      cToken = canvas.tokens.get(fromUuidSync(card.dataset.tokenId)._id);
+      cToken.control({ releaseOthers: true });
+      await act.rollFumble(options);
+      canvas.tokens.selectObjects();
       for (const token of controlledTokens) {
-        cToken = canvas.tokens.get(token.id)
-        cToken.control({ releaseOthers: false })
-      }      
-      break
-    case 'crit':
-      cToken = canvas.tokens.get(fromUuidSync(card.dataset.tokenId)._id)
-      cToken.control({ releaseOthers: true })
-      await DCCQOLactor.rollCriticalQOL(options, targettoken)
-      canvas.tokens.selectObjects()
-      for (const token of controlledTokens) {
-        cToken = canvas.tokens.get(token.id)
-        cToken.control({ releaseOthers: false })
+        cToken = canvas.tokens.get(token.id);
+        cToken.control({ releaseOthers: false });
       }
-      break
-    case 'friendlyFire':
-
-      const roll = new Roll('d100')
+      break;
+    case "crit":
+      cToken = canvas.tokens.get(fromUuidSync(card.dataset.tokenId)._id);
+      cToken.control({ releaseOthers: true });
+      await DCCQOLactor.rollCriticalQOL(options, targettoken);
+      canvas.tokens.selectObjects();
+      for (const token of controlledTokens) {
+        cToken = canvas.tokens.get(token.id);
+        cToken.control({ releaseOthers: false });
+      }
+      break;
+    case "friendlyFire":
+      const roll = new Roll("d100");
       await roll.evaluate({
-        async: true
-      })
+        async: true,
+      });
 
-      const friendlyFire = await roll.render()
-      let friendlyFireHTML
+      const friendlyFire = await roll.render();
+      let friendlyFireHTML;
 
       if (roll._total >= 51) {
-        const chatText = game.i18n.format('DCC-QOL.FriendlyFireSuccess', {
-          weapon: weapon.name
-        })
-        friendlyFireHTML = friendlyFire.replace('dice-total', 'dice-total success') + `<div class="dccqol chat-card"><div class="chat-details"><div class="ff-result">${chatText}</div></div></div>`
+        const chatText = game.i18n.format("DCC-QOL.FriendlyFireSuccess", {
+          weapon: weapon.name,
+        });
+        friendlyFireHTML =
+          friendlyFire.replace("dice-total", "dice-total success") +
+          `<div class="dccqol chat-card"><div class="chat-details"><div class="ff-result">${chatText}</div></div></div>`;
       } else {
-        const chatText = game.i18n.format('DCC-QOL.FriendlyFireFail', {
-          weapon: weapon.name
-        })
-        friendlyFireHTML = friendlyFire.replace('dice-total', 'dice-total fail') + `<div class="dccqol chat-card"><div class="chat-details"><div class="ff-result">${chatText}</div></div></div>`
+        const chatText = game.i18n.format("DCC-QOL.FriendlyFireFail", {
+          weapon: weapon.name,
+        });
+        friendlyFireHTML =
+          friendlyFire.replace("dice-total", "dice-total fail") +
+          `<div class="dccqol chat-card"><div class="chat-details"><div class="ff-result">${chatText}</div></div></div>`;
       }
 
       roll.toMessage({
         user: game.user.id,
         speaker: {
-          alias: actor.name
+          alias: actor.name,
         },
-        flavor: game.i18n.localize('DCC-QOL.FriendlyFireCheck'),
+        flavor: game.i18n.localize("DCC-QOL.FriendlyFireCheck"),
         content: friendlyFireHTML,
-        rollMode: game.settings.get('core', 'rollMode')
-      })
-      break
+        rollMode: game.settings.get("core", "rollMode"),
+      });
+      break;
   }
 }
--- scripts/dcc-qol.js
+++ scripts/dcc-qol.js
@@ -1,70 +1,79 @@
 /* global canvas, game, Hooks, libWrapper, loadTemplates, ui */
-import {
-  registerSystemSettings
-} from './settings.js'
-import DCCQOL from './patch.js'
-import * as chat from './chat.js'
-export async function preloadTemplates () {
-  const templatePaths = ['modules/dcc-qol/templates/attackroll-card.html']
-  return loadTemplates(templatePaths)
+import { registerSystemSettings } from "./settings.js";
+import DCCQOL from "./patch.js";
+import * as chat from "./chat.js";
+export async function preloadTemplates() {
+  const templatePaths = ["modules/dcc-qol/templates/attackroll-card.html"];
+  return loadTemplates(templatePaths);
 }
 
-function tokenForActorId (actorId) {
-  const actor = game.actors.get(actorId)
-  const allTokens = actor.getActiveTokens()
-  if (allTokens.length === 1) return allTokens[0].document
+function tokenForActorId(actorId) {
+  const actor = game.actors.get(actorId);
+  const allTokens = actor.getActiveTokens();
+  if (allTokens.length === 1) return allTokens[0].document;
   else {
-    const controlled = canvas?.tokens?.controlled
-    const filteredTokens = allTokens.filter(value => controlled.includes(value))
-    if (filteredTokens.length === 1) return filteredTokens[0].document
+    const controlled = canvas?.tokens?.controlled;
+    const filteredTokens = allTokens.filter((value) =>
+      controlled.includes(value)
+    );
+    if (filteredTokens.length === 1) return filteredTokens[0].document;
     else {
-      return undefined
+      return undefined;
     }
   }
 }
 
-function rollPatchedWeaponAttack (wrapped, ...args) {
-  const actor = new DCCQOL(this)
-  const tokenD = tokenForActorId(this._id)
-  if (tokenD) {
-    actor.rollWeaponAttackQOL(args[0], args[1], tokenD)
-  } else {
-    return ui.notifications.warn(game.i18n.localize('DCC-QOL.ControlAToken'))
+function rollPatchedWeaponAttack(wrapped, ...args) {
+  const actor = new DCCQOL(this);
+  const tokenD = tokenForActorId(this._id);
+  // if settings are set to checkWeaponRange, or automateFriendlyFire, require a token to be in scene; otherwise, proceed without one
+  if (
+    game.settings.get("dcc-qol", "checkWeaponRange") ||
+    game.settings.get("dcc-qol", "automateFriendlyFire")
+  ) {
+    if (!tokenD) {
+      return ui.notifications.warn(game.i18n.localize("DCC-QOL.ControlAToken"));
+    }
   }
+  actor.rollWeaponAttackQOL(args[0], args[1], tokenD);
 }
 
-function initPatching () {
-  libWrapper.register('dcc-qol', 'game.dcc.DCCActor.prototype.rollWeaponAttack', rollPatchedWeaponAttack, 'MIXED')
+function initPatching() {
+  libWrapper.register(
+    "dcc-qol",
+    "game.dcc.DCCActor.prototype.rollWeaponAttack",
+    rollPatchedWeaponAttack,
+    "MIXED"
+  );
 }
 
-Hooks.once('init', async function () {
-  console.log('DCC-QOL | Initializing DCC-QOL.')
-  if (!game.modules.get('lib-wrapper')?.active) {
-    console.warn('DCC-QOL | libWrapper is NOT active exiting!')
+Hooks.once("init", async function () {
+  console.log("DCC-QOL | Initializing DCC-QOL.");
+  if (!game.modules.get("lib-wrapper")?.active) {
+    console.warn("DCC-QOL | libWrapper is NOT active exiting!");
   }
-  if (!game.modules.get('socketlib')?.active) {
-    console.warn('DCC-QOL | socketlib is NOT active exiting!')
-    return
+  if (!game.modules.get("socketlib")?.active) {
+    console.warn("DCC-QOL | socketlib is NOT active exiting!");
+    return;
   }
-  await registerSystemSettings()
-  preloadTemplates()
-  initPatching()
-  Hooks.on('renderChatLog', (app, html, data) => chat.addChatListeners(html))
-})
+  await registerSystemSettings();
+  preloadTemplates();
+  initPatching();
+  Hooks.on("renderChatLog", (app, html, data) => chat.addChatListeners(html));
+});
 
-Hooks.once('setup', async function () {
+Hooks.once("setup", async function () {
   // Do anything after initialization but before ready
-  chat.setupSocket()
-}
-)
+  chat.setupSocket();
+});
 
-Hooks.once('ready', async function () {
-  if (!game.modules.get('lib-wrapper')?.active && game.user.isGM) {
-    console.warn('DCC-QOL | libWrapper is NOT active exiting!')
-    ui.notifications.warn(game.i18n.localize('DCC-QOL.libwrapperWarning'))
+Hooks.once("ready", async function () {
+  if (!game.modules.get("lib-wrapper")?.active && game.user.isGM) {
+    console.warn("DCC-QOL | libWrapper is NOT active exiting!");
+    ui.notifications.warn(game.i18n.localize("DCC-QOL.libwrapperWarning"));
   }
-  if (!game.modules.get('socketlib')?.active && game.user.isGM) {
-    console.warn('DCC-QOL | socketlib is NOT active exiting!')
-    ui.notifications.warn(game.i18n.localize('DCC-QOL.socketlibWarning'))
+  if (!game.modules.get("socketlib")?.active && game.user.isGM) {
+    console.warn("DCC-QOL | socketlib is NOT active exiting!");
+    ui.notifications.warn(game.i18n.localize("DCC-QOL.socketlibWarning"));
   }
-})
+});
